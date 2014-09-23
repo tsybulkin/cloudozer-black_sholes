66c2ce1ab08c32f6d09a80d457b2414fd1e433b2
Ian Tsybulkin <tsybulkin@Ians-MacBook-Air.local>
2014-09-23 10:02:19 +0300

    secant method added

--- iv.erl
+++ iv.erl
@@ -33,9 +33,9 @@ implicit_vol(Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike,Option) ->
 
 	case {Fa < 0, Fb > 0} of
 		{true, true} -> % start find volatility
-			bisect(1,A,B,Func);
+			secant(1,A,B,Func);
 		{false,false}-> % swap and start 
-			bisect(1,B,A,Func);
+			secant(1,B,A,Func);
 		_ -> no_roots
 	end.
 
@@ -53,7 +53,26 @@ bisect(Iter, A,B, Func) ->
 			io:format("Found in ~p iterations~n",[Iter]),
 			C  
 	end.
-	
+
+secant(?MAX_ITER,A,B,Func) -> 
+	Fa = Func(A),
+	Fb = Func(B),
+	(A*Fb-B*Fa)/(Fb-Fa);
+
+secant(Iter, A,B, Func) ->
+	Fa = Func(A),
+	Fb = Func(B),
+	C = (A*Fb-B*Fa)/(Fb-Fa),
+	Fc = Func(C),
+	if
+		Fc > ?VOL_TOL ->
+			secant(Iter+1, A, C, Func);
+		Fc < -?VOL_TOL ->
+			secant(Iter+1, C, B, Func);
+		true ->
+			io:format("Found in ~p iterations~n",[Iter]),
+			C  
+	end.
 
 
 
