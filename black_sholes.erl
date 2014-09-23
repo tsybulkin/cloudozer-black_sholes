198beed8d645f759460dd5aad9f8e98b65ad8aac
Ian Tsybulkin <tsybulkin@Ians-MacBook-Air.local>
2014-09-23 09:35:19 +0300

    implicit volatility developed.Algo uses bisection method

--- black_sholes.erl
+++ black_sholes.erl
@@ -24,16 +24,16 @@ blash(Vol,Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike) ->
 
     D1 = ((LogSK + (Roll - Yield) * Cttx) / St) + (0.5 * St),
     D2 = D1 - St,
-    io:format("D1:~p, D2:~p~n",[D1,D2]),
+    %io:format("D1:~p, D2:~p~n",[D1,D2]),
     Nd1 = ?INV_SQRTPI * math:exp(-0.5 * D1 * D1),
-    io:format("nd1:~p~n",[Nd1]),
+    %io:format("nd1:~p~n",[Nd1]),
 
     get_greeks(CallPut,D1,D2,Spot,Bor_div_disc,Box_disc,Nd1,St,SqrtVttx,Cttx,Strike,Vol).
 
 get_greeks(option_call,D1,D2,Spot,Bor_div_disc,Box_disc,Nd1,St,SqrtVttx,Cttx,Strike,Vol) ->
 	N1 = gaussian:gaussianCDF(D1),
     N2 = gaussian:gaussianCDF(D2),
-    io:format("N1:~p, N2:~p~n~n",[N1,N2]),
+    %io:format("N1:~p, N2:~p~n~n",[N1,N2]),
     TV = Spot * Bor_div_disc * N1 - Strike * Box_disc * N2,
     Delta = Bor_div_disc * N1,
     Gamma = Nd1 * Bor_div_disc / (Spot * St),
@@ -42,14 +42,14 @@ get_greeks(option_call,D1,D2,Spot,Bor_div_disc,Box_disc,Nd1,St,SqrtVttx,Cttx,Str
     BorrowRho = - Cttx * Spot * Delta,
     Vegavega = Vega * D1 * D2 / Vol,
     Deltavega = - Bor_div_disc * D2 * Nd1 / Vol,
-    io:format("TV:~p~nDelta:~p~nGamma:~p~nVega:~p~nBoxRho:~p~nBorrowRho:~p~nVegaVega:~p~nDeltaVega:~p~n",
-    	[TV,Delta,Gamma,Vega,BoxRho,BorrowRho,Vegavega,Deltavega]),
+    %io:format("TV:~p~nDelta:~p~nGamma:~p~nVega:~p~nBoxRho:~p~nBorrowRho:~p~nVegaVega:~p~nDeltaVega:~p~n",
+    %	[TV,Delta,Gamma,Vega,BoxRho,BorrowRho,Vegavega,Deltavega]),
     {TV,Delta,Gamma,Vega,BoxRho,BorrowRho,Vegavega,Deltavega};
 
 get_greeks(option_put,D1,D2,Spot,Bor_div_disc,Box_disc,Nd1,St,SqrtVttx,Cttx,Strike,Vol) ->
 	N1 = gaussian:gaussianCDF(-D1),
     N2 = gaussian:gaussianCDF(-D2),
-    io:format("N1:~p, N2:~p~n~n",[N1,N2]),
+    %io:format("N1:~p, N2:~p~n~n",[N1,N2]),
     TV = - Spot * Bor_div_disc * N1 + Strike * Box_disc * N2,
     Delta = - Bor_div_disc * N1,
     Gamma = Nd1 * Bor_div_disc / (Spot * St),
@@ -58,8 +58,8 @@ get_greeks(option_put,D1,D2,Spot,Bor_div_disc,Box_disc,Nd1,St,SqrtVttx,Cttx,Stri
     BorrowRho = - Cttx * Spot * Delta,
     Vegavega = Vega * D1 * D2 / Vol,
     Deltavega = - Bor_div_disc * D2 * Nd1 / Vol,
-    io:format("TV:~p~nDelta:~p~nGamma:~p~nVega:~p~nBoxRho:~p~nBorrowRho:~p~nVegaVega:~p~nDeltaVega:~p~n",
-    	[TV,Delta,Gamma,Vega,BoxRho,BorrowRho,Vegavega,Deltavega]),
+    %io:format("TV:~p~nDelta:~p~nGamma:~p~nVega:~p~nBoxRho:~p~nBorrowRho:~p~nVegaVega:~p~nDeltaVega:~p~n",
+    %	[TV,Delta,Gamma,Vega,BoxRho,BorrowRho,Vegavega,Deltavega]),
     {TV,Delta,Gamma,Vega,BoxRho,BorrowRho,Vegavega,Deltavega};
 
 get_greeks(_,_,_,_,_,_,_,_,_,_,_,_) -> bad_input.
--- iv.erl
+++ iv.erl
@@ -3,50 +3,54 @@
 %
 
 -module(iv).
--export([implicit_vol/1]).
+-export([test/0]).
 
 -define(VOL_GUESS_MIN, 0.05).
 -define(VOL_GUESS_MAX, 2.5).
 -define(MAX_ITER, 100).
 -define(VOL_TOL, 0.001).
 
-x() -> {100.0, 1.0, 1.0, 0.01, 0, 0, option_call,100.0}.
+x() -> {0.5,100.0, 1.0, 1.0, 0.01, 0, 0, option_call,100.0}.
 
-tv(Vol) ->
-	{Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike} = x(),
-	{TV,_,_,_,_,_,_,_} = black_sholes:blash(Vol,Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike), 
-	TV.
+test() ->
+	{Vol,Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike} = x(),
+	io:format("Correct volatility: ~p~n",[Vol]),
+	{Opt,_,_,_,_,_,_,_} = black_sholes:blash(Vol,Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike),
+	IV = implicit_vol(Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike,Opt),
+	io:format("Implicit volatility: ~p~n",[IV]).
 
-f(Vol,True_Vol) ->
-	tv(Vol) - tv(True_Vol).
 
-implicit_vol(Vol) ->
-	{Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike} = x(),
-	TV = tv(0.5),
-	io:format("Volatility:~p~n",[Vol]),
+implicit_vol(Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike,Option) ->
+	Func = fun(V)->
+		{Opt,_,_,_,_,_,_,_} = black_sholes:blash(V,Spot,Cttx,Vttx,Box,Borrow,Yield,CallPut,Strike),
+		Opt - Option
+	end,
 
 	A = ?VOL_GUESS_MIN,
 	B = ?VOL_GUESS_MAX,
-	case f(A,TV)*f(B,TV) > 0 of
-		true -> no_roots;
-		_ -> implicit_vol(1, A,B,fun(V)-> f(V,Vol) end)
+	Fa = Func(A),
+	Fb = Func(B),
 
+	case {Fa < 0, Fb > 0} of
+		{true, true} -> % start find volatility
+			bisect(1,A,B,Func);
+		{false,false}-> % swap and start 
+			bisect(1,B,A,Func);
+		_ -> no_roots
 	end.
 
 	
-implicit_vol(?MAX_ITER,A,B,_) -> (A+B)*0.5;
-implicit_vol(Iter, A,B, Func) ->
-	Fa = Func(A),
+bisect(?MAX_ITER,A,B,_) -> (A+B)*0.5;
+bisect(Iter, A,B, Func) ->
 	C = (A+B)*0.5,
 	Fc = Func(C),
-	Fac = Fa*Fc,
-
 	if
-		Fac > ?VOL_TOL ->
-			implicit_vol(Iter+1, B, C, Func);
-		Fac < -?VOL_TOL ->
-			implicit_vol(Iter+1, A, C, Func);
+		Fc > ?VOL_TOL ->
+			bisect(Iter+1, A, C, Func);
+		Fc < -?VOL_TOL ->
+			bisect(Iter+1, C, B, Func);
 		true ->
+			io:format("Found in ~p iterations~n",[Iter]),
 			C  
 	end.
 	
